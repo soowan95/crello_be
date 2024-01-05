a3d3281e1ef842553bf8a11ae7c7f0d424e7f7d7
soowan95 <ksw9565@gmail.com>
2024-01-05 23:40:13 +0900

    modify/querydsl 생성 디렉토리 변경

--- src/main/java/com/v1/crello/board/querydsl/BoardRepositoryCustomImpl.java
+++ src/main/java/com/v1/crello/board/querydsl/BoardRepositoryCustomImpl.java
@@ -1,8 +1,8 @@
 package com.v1.crello.board.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.v1.crello.entity.Board;
 import com.v1.crello.entity.QBoard;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
